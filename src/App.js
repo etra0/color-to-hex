5311eef799c2e279e85b63ccac5915ace5810d4a
Sebastián Aedo <saedo@alumnos.inf.utfsm.cl>
2020-04-09 20:18:09 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import { SketchPicker } from 'react-color';
+import {write} from 'ieee754';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
 import './App.css';
 
+const theme = createMuiTheme({
+	  palette: {
+			    primary: purple,
+			    secondary: green,
+					type: 'dark'
+			  },
+	  status: {
+			    danger: 'orange',
+			  },
+});
+
 function App() {
+	const [color, setColor] = useState({r: 0, g: 0, b: 0, a: 0});
+	const [endianness, setEndianness] = useState('little')
+
+	const generate_aob = color => {
+		const _color = JSON.parse(JSON.stringify(color));
+		['r', 'g', 'b'].map(v => {_color[v] = (_color[v]/255)});
+		_color.a = (_color.a);
+		return ['a', 'r', 'g', 'b'].reduce((acc, v) => {
+			let b = [0, 0, 0, 0];
+			write(b, _color[v], 0, endianness === 'little', 23, 4);
+			b = b.map(v => v.toString(16)).join('');
+			return acc + ' ' + b;
+
+		}, '');
+
+	}
+
   return (
+		<ThemeProvider theme={theme}>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+				<div>
+					<SketchPicker
+						color={ color }
+						onChange={ c => setColor(c.rgb) } />
+				</div>
+				<div style={{margin: 20}}>
+					<InputLabel>Endianness</InputLabel>
+					<Select
+						value={endianness}
+						onChange={v => setEndianness(v.target.value)} >
+						<MenuItem value='little'>Little-endian</MenuItem>
+						<MenuItem value='big'>Big-endian</MenuItem>
+					</Select>
+				</div>
+				<p>Copy the bytes</p>
+				<p>{generate_aob(color)}</p>
       </header>
     </div>
+		</ThemeProvider>
   );
 }
 
