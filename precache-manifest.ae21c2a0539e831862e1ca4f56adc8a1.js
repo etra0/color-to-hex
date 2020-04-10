9c5d9d9e05d73e955d06969092c6fca970ae31e8
Sebastián Aedo <saedo@alumnos.inf.utfsm.cl>
2020-04-09 20:21:08 -0400

    Updates

---
+++ precache-manifest.ae21c2a0539e831862e1ca4f56adc8a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bc1d25e512a0e5cacffe57e02583e5b",
+    "url": "/color-to-hex/index.html"
+  },
+  {
+    "revision": "efcb36f9b8c710edeb21",
+    "url": "/color-to-hex/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ff13a53a1e948131c3df",
+    "url": "/color-to-hex/static/js/2.b48f5b54.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/color-to-hex/static/js/2.b48f5b54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efcb36f9b8c710edeb21",
+    "url": "/color-to-hex/static/js/main.c7edca9b.chunk.js"
+  },
+  {
+    "revision": "a0b1622089cf8349333a",
+    "url": "/color-to-hex/static/js/runtime-main.af8267a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/color-to-hex/static/media/logo.5d5d9eef.svg"
+  }
+]);
